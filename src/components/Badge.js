3860616957f3ec8eb74ab8112c053980bbd967cc
Israel Castro <israel.castro@gmail.com>
2020-07-26 16:55:33 -0500

    4. Estilos

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,29 +1,34 @@
 import React from "react";
 
+import "./styles/Badge.css";
 import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="Badge">
+        <div className="Badge__header">
           <img src={confLogo} alt="Logo de la conferencia" />
         </div>
 
-        <div>
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://es.gravatar.com/userimage/15984944/d3537319e5b8b364becbc34517450e40.jpg?size=200"
+            alt="Avatar"
+          />
           <h1>
             Israel <br />
             Castro
           </h1>
         </div>
 
-        <div>
-          <p>Frontend Engineer</p>
-          <p>@iscasur</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Engineer</h3>
+          <div>@iscasur</div>
         </div>
 
-        <div>#PlatziConf</div>
+        <div className="Badge__footer">#PlatziConf</div>
       </div>
     );
   }
