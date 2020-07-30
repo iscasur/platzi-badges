87a9073f33f38e30c652c41b530bf7b2b9fbd8b8
Israel Castro <israel.castro@gmail.com>
2020-07-30 02:57:58 -0500

    6. Nuestra primera pagina

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./global.css";
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 const container = document.getElementById("app");
 
-ReactDOM.render(
-  <Badge
-    firstName="Israel"
-    lastName="Castro"
-    avatarUrl="https://es.gravatar.com/userimage/15984944/d3537319e5b8b364becbc34517450e40.jpg?size=200"
-    jobTitle="Frontend Enginer"
-    twitter="iscasur"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
