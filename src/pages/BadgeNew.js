9511fb3f69ae1663763c99fadd7b92c3347d23b5
Israel Castro <israel.castro@gmail.com>
2020-08-07 00:59:08 -0500

    12. Mejorando la UI con un layout

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import "./styles/BadgeNew.css";
 import header from "../images/badge-header.svg";
-import Navbar from "../components/Navbar";
 import BadgeForm from "../components/BadgeForm";
 import Badge from "../components/Badge";
 
@@ -31,8 +30,7 @@ class BadgeNew extends React.Component {
 
   render() {
     return (
-      <div>
-        <Navbar />
+      <React.Fragment>
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="Logo" />
         </div>
@@ -57,7 +55,7 @@ class BadgeNew extends React.Component {
             </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
