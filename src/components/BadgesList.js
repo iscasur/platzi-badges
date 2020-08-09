01356f83f6d5e4e4a0b9ea65bf9f8d486195d3f8
Israel Castro <israel.castro@gmail.com>
2020-08-09 14:35:39 -0500

    16. Creando nuevos badges

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./styles/BadgesList.css";
+import Gravatar from "./Gravatar";
 
 class BadgesListItem extends React.Component {
   render() {
     return (
       <div className="BadgesListItem">
-        <img
+        <Gravatar
           className="BadgesListItem__avatar"
-          src={this.props.badge.avatarUrl}
+          email={this.props.badge.email}
           alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
         />
 
@@ -28,7 +29,7 @@ class BadgesListItem extends React.Component {
 
 class BadgesList extends React.Component {
   render() {
-    if(this.props.badges.length === 0) {
+    if (this.props.badges.length === 0) {
       return (
         <div>
           <h3>No badges were found</h3>
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 import "./styles/BadgeNew.css";
-import header from "../images/badge-header.svg";
+import header from "../images/platziconf-logo.svg";
 import BadgeForm from "../components/BadgeForm";
 import Badge from "../components/Badge";
+import api from "../api";
 
 class BadgeNew extends React.Component {
   state = {
@@ -17,9 +18,6 @@ class BadgeNew extends React.Component {
   };
 
   handleChange = (e) => {
-    // const nextForm = this.state.form;
-    // nextForm[e.target.name] = e.target.value;
-
     this.setState({
       form: {
         ...this.state.form,
@@ -28,28 +26,45 @@ class BadgeNew extends React.Component {
     });
   };
 
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    this.setState({ loading: true, error: null });
+
+    try {
+      await api.badges.create(this.state.form);
+      this.setState({ loading: false });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="BadgeNew__hero">
-          <img className="img-fluid" src={header} alt="Logo" />
+          <img
+            className="BadgeNew__hero-image img-fluid"
+            src={header}
+            alt="Logo"
+          />
         </div>
 
         <div className="container">
           <div className="row">
             <div className="col-6">
               <Badge
-                firstName={this.state.form.firstName}
-                lastName={this.state.form.lastName}
-                twitter={this.state.form.twitter}
-                jobTitle={this.state.form.jobTitle}
-                email={this.state.form.email}
+                firstName={this.state.form.firstName || "FIRST_NAME"}
+                lastName={this.state.form.lastName || "LAST_NAME"}
+                twitter={this.state.form.twitter || "twitter"}
+                jobTitle={this.state.form.jobTitle || "JOB_TITLE"}
+                email={this.state.form.email || "EMAIL"}
                 avatarUrl="https://www.gravatar.com/avatar/e77a4e18cccdc0e7ecc6235afae39a5f?s=200"
               />
             </div>
             <div className="col-6">
               <BadgeForm
                 onChange={this.handleChange}
+                onSubmit={this.handleSubmit}
                 formValues={this.state.form}
               />
             </div>
