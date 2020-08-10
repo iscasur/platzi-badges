0aa4c5ae5962fc01ab51fe0b6053e418da3f0f1e
Israel Castro <israel.castro@gmail.com>
2020-08-10 01:32:28 -0500

    21. Contenedores, componentes y 22. Portales -> modales

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,95 +1,66 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 
 import "./styles/BadgeDetails.css";
 import confLogo from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge";
-import api from "../api";
-import PageLoading from "../components/PageLoading";
-import PageError from "../components/PageError";
 
-class BadgeDetails extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    data: undefined,
-  };
+function BadgeDetails(props) {
+  const badge = props.badge;
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = async () => {
-    this.setState({ loading: true, error: null });
-
-    try {
-      const data = await api.badges.read(this.props.match.params.badgeId);
-      this.setState({ loading: false, data: data });
-    } catch (error) {
-      this.setState({ loading: false, error: error });
-    }
-  };
-
-  render() {
-    if (this.state.loading) {
-      return <PageLoading />;
-    }
-
-    if (this.state.error) {
-      return <PageError error={this.state.error} />;
-    }
-
-    const badge = this.state.data;
-
-    return (
-      <div>
-        <div className="BadgeDetails__hero">
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <img src={confLogo} alt="Logo de la Conferencia" />
-              </div>
-              <div className="col-6 BadgeDetails__hero-attendant-name">
-                <h1>
-                  {badge.firstName} {badge.firstName}
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-
+  return (
+    <div>
+      <div className="BadgeDetails__hero">
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <Badge
-                firstName={badge.firstName}
-                lastName={badge.lastName}
-                email={badge.email}
-                twitter={badge.twitter}
-                jobTitle={badge.jobTitle}
-              />
+              <img src={confLogo} alt="Logo de la Conferencia" />
             </div>
-            <div className="col-6">
-              <h2>Actions</h2>
+            <div className="col-6 BadgeDetails__hero-attendant-name">
+              <h1>
+                {badge.firstName} {badge.firstName}
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <Badge
+              firstName={badge.firstName}
+              lastName={badge.lastName}
+              email={badge.email}
+              twitter={badge.twitter}
+              jobTitle={badge.jobTitle}
+            />
+          </div>
+          <div className="col-6">
+            <h2>Actions</h2>
+            <div>
               <div>
-                <div>
-                  <Link
-                    className="btn btn-primary mb-4"
-                    to={`/badges/${badge.id}/edit`}
-                  >
-                    Edit
-                  </Link>
-                </div>
-                <div>
-                  <button class="btn btn-danger">Delete</button>
-                </div>
+                <Link
+                  className="btn btn-primary mb-4"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  Edit
+                </Link>
+              </div>
+              <div>
+                <button class="btn btn-danger">Delete</button>
+                {ReactDOM.createPortal(
+                  <h1>Hola, realmente no estoy aquí</h1>,
+                  document.getElementById("modal")
+                )}
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default BadgeDetails;
